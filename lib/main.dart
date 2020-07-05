4b4010edc27cabc76fd047b80382afd3ee4fb834
apphow <apehow111@gmail.com>
2020-07-05 01:32:23 -0400

    added Expanded Widgets to FlatButtons to spread equally vertically

--- lib/main.dart
+++ lib/main.dart
@@ -17,47 +17,61 @@ class XylophoneApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: <Widget>[
-              FlatButton(
-                color: Colors.red[900],
-                onPressed: () {
-                  playsound(1);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.red[900],
+                  onPressed: () {
+                    playsound(1);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.teal,
-                onPressed: () {
-                  playsound(2);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.teal,
+                  onPressed: () {
+                    playsound(2);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.yellow[700],
-                onPressed: () {
-                  playsound(3);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.yellow[700],
+                  onPressed: () {
+                    playsound(3);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.orange[700],
-                onPressed: () {
-                  playsound(4);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.orange[700],
+                  onPressed: () {
+                    playsound(4);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.deepPurple[300],
-                onPressed: () {
-                  playsound(5);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.deepPurple[300],
+                  onPressed: () {
+                    playsound(5);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.blueAccent[700],
-                onPressed: () {
-                  playsound(6);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.blueAccent[700],
+                  onPressed: () {
+                    playsound(6);
+                  },
+                ),
               ),
-              FlatButton(
-                color: Colors.pink[300],
-                onPressed: () {
-                  playsound(7);
-                },
+              Expanded(
+                child: FlatButton(
+                  color: Colors.pink[300],
+                  onPressed: () {
+                    playsound(7);
+                  },
+                ),
               ),
             ],
           ),
