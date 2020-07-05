f182ef71d9f76cf48fac59f0d7c7567809e47cd6
apphow <apehow111@gmail.com>
2020-07-05 02:18:01 -0400

    created a build key to get rid of redundant code for FlatButtons

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,20 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-  void playsound(int soundNumber) {
+  void playSound(int soundNumber) {
     final player = AudioCache();
     player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
   }
 
   @override
@@ -18,80 +29,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Expanded(
-                child: FlatButton(
-                  color: Colors.red[900],
-                  onPressed: () {
-                    playsound(1);
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.teal,
-                  onPressed: () {
-                    playsound(2);
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.yellow[700],
-                  onPressed: () {
-                    playsound(3);
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.orange[700],
-                  onPressed: () {
-                    playsound(4);
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.deepPurple[300],
-                  onPressed: () {
-                    playsound(5);
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.blueAccent[700],
-                  onPressed: () {
-                    playsound(6);
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.pink[300],
-                  onPressed: () {
-                    playsound(7);
-                  },
-                ),
-              ),
+              buildKey(color: Colors.red[900], soundNumber: 1),
+              buildKey(color: Colors.teal, soundNumber: 2),
+              buildKey(color: Colors.yellow[700], soundNumber: 3),
+              buildKey(color: Colors.orange[700], soundNumber: 4),
+              buildKey(color: Colors.deepPurple[300], soundNumber: 5),
+              buildKey(color: Colors.blueAccent[700], soundNumber: 6),
+              buildKey(color: Colors.pink[300], soundNumber: 7),
             ],
           ),
         ),
